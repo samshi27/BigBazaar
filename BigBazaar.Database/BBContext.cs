732883a57e836eb64883f1efdc475d2afeb63cee
samshi27 <samshi.c@gmail.com>
2021-07-27 19:25:52 +0530

    Add project files.

---
+++ BigBazaar.Database/BBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+using BigBazaar.Entities;
+
+namespace BigBazaar.Database
+{
+    public class BBContext : DbContext 
+    {
+        public BBContext(): base("BigBazaarConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BigBazaar.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace BigBazaar.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BigBazaar.Database.BBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BigBazaar.Database.BBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
